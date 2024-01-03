925f46a66b49f42f589d9446e4b7f29ac4eb2615
saturn30 <soso01dev@gmail.com>
2024-01-03 23:15:24 +0900

    feat: drizzle 설치

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+const app = new Hono();
 
-const app = new Hono()
+app.get("/", (c) => c.text("Hello Hono!"));
 
-app.get('/', (c) => c.text('Hello Hono!'))
-
-export default app
+export default app;
