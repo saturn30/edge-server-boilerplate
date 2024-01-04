0b1a4a0380ea051698843b341af3eab84307c75a
saturn30 <soso01dev@gmail.com>
2024-01-04 23:07:47 +0900

    feat: 서비스 인스턴스 생성, 재사용

--- src/features/todo/todo.routes.ts
+++ src/features/todo/todo.routes.ts
@@ -1,41 +1,41 @@
 import { createApp } from "../../utils/createApp";
 import { TodoService } from "./todo.service";
 
-export const todoRoutes = createApp();
+export const todoRoutes = createApp<{ todoService: TodoService }>();
+
+// 서비스 인스턴스 생성 후 var에 할당
+todoRoutes.use(async (c, next) => {
+  c.set("todoService", new TodoService());
+  await next();
+});
 
 todoRoutes.get("/", (c) => {
-  const todoService = new TodoService();
-
-  return c.json(todoService.getAll());
+  return c.json(c.var.todoService.getAll());
 });
 
 todoRoutes.get("/:id", (c) => {
-  const todoService = new TodoService();
   const { id } = c.req.param();
 
-  return c.json(todoService.getItem(Number(id)));
+  return c.json(c.var.todoService.getItem(Number(id)));
 });
 
 todoRoutes.post("/", async (c) => {
-  const todoService = new TodoService();
   const body = await c.req.json<{ title: string }>();
-  todoService.add(body.title);
+  c.var.todoService.add(body.title);
 
   return c.json({ result: "done" });
 });
 
 todoRoutes.put("/:id", async (c) => {
-  const todoService = new TodoService();
   const { id } = c.req.param();
-  todoService.toggle(Number(id));
+  c.var.todoService.toggle(Number(id));
 
   return c.json({ result: "done" });
 });
 
 todoRoutes.delete("/:id", async (c) => {
-  const todoService = new TodoService();
   const { id } = c.req.param();
-  todoService.delete(Number(id));
+  c.var.todoService.delete(Number(id));
 
   return c.json({ result: "done" });
 });
--- src/utils/createApp.ts
+++ src/utils/createApp.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { Bindings } from "../types/app";
+import { Variables } from "hono/types";
 
-export const createApp = () => {
-  return new Hono<{ Bindings: Bindings }>();
+export const createApp = <T extends Variables>() => {
+  return new Hono<{ Bindings: Bindings; Variables: T }>();
 };
