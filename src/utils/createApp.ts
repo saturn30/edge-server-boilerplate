3b5af67f8f9162452b1e16b23cd71994c465658b
saturn30 <soso01dev@gmail.com>
2024-01-05 21:07:38 +0900

    feat: biome 설치

--- src/utils/createApp.ts
+++ src/utils/createApp.ts
@@ -3,5 +3,5 @@ import { Bindings } from "../types/app";
 import { Variables } from "hono/types";
 
 export const createApp = <T extends Variables>() => {
-  return new Hono<{ Bindings: Bindings; Variables: T }>();
+	return new Hono<{ Bindings: Bindings; Variables: T }>();
 };
