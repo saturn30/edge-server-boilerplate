5049c2885e20b78fad53da41d3c11fe147315af0
saturn30 <soso01dev@gmail.com>
2024-01-11 23:20:54 +0900

    feat: getGoogleUser 메서드 추가

--- src/features/auth/auth.service.ts
+++ src/features/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { google } from "worker-auth-providers";
+import { GoogleUser } from "./auth.types";
 
 export class AuthService {
   constructor(
@@ -18,5 +19,18 @@ export class AuthService {
         redirectTo: this.authKey.google.redirectUrl,
       },
     });
+  };
+
+  getGoogleUser = async (request: Request) => {
+    const { user }: { user: GoogleUser } = await google.users({
+      options: {
+        clientId: this.authKey.google.clientId,
+        clientSecret: this.authKey.google.clientSecret,
+        redirectUrl: this.authKey.google.redirectUrl,
+      },
+      request,
+    });
+
+    return { user };
   };
 }
---
+++ src/features/auth/auth.types.ts
@@ -0,0 +1,6 @@
+export interface GoogleUser {
+  id: string;
+  email: string;
+  name: string;
+  picture: string;
+}
