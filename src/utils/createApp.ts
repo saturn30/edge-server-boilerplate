e53c6b958c589c41ddb82313c59d155106dfdd80
saturn30 <soso01dev@gmail.com>
2024-01-13 21:22:50 +0900

    feat: userId variable 전역 설정

--- src/utils/createApp.ts
+++ src/utils/createApp.ts
@@ -4,5 +4,5 @@ import { Variables } from "hono/types";
 import { Bindings } from "~/types/app";
 
 export const createApp = <T extends Variables>() => {
-  return new Hono<{ Bindings: Bindings; Variables: T }>();
+  return new Hono<{ Bindings: Bindings; Variables: T & { userId: number } }>(); // 매번 타입 설정이 귀찮으므로 userId는 전역으로 설정함.
 };
